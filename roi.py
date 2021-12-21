0004c0fe3f688cc4d7ffd101b3943ddbea1098cc
DevAtDawn <86164137+DevAtDawn@users.noreply.github.com>
2021-12-21 06:22:05 -0700

    Create roi.py

---
+++ roi.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+
+def select_roi()
+    # Read image
+    im = cv2.imread("pic.png")
+    # im = cv2.imread("image.jpg")
+
+    # Select ROI
+    r = cv2.selectROI(im)
+    print(r)
+    # Crop image
+    imCrop = im[int(r[1]):int(r[1]+r[3]), int(r[0]):int(r[0]+r[2])]
+ 
+    # Display cropped image
+    cv2.imshow("Image", imCrop)
+    cv2.waitKey(0)
+    
+if __name__ == '__main__'
+    select_roi()
